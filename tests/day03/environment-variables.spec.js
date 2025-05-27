8f01eb471e148c0b7b1ecb6238884e2666309a51
umitenes <53514626+umitenes@users.noreply.github.com>
2025-05-27 03:52:34 -0700

    Update environment-variables.spec.js

--- tests/day03/environment-variables.spec.js
+++ tests/day03/environment-variables.spec.js
@@ -1,13 +1,1 @@
-import {test}from '@playwright/test';
-test('Environment Variables @Env-test', async ({ page }) => {
-   console.log(process.env.PRACTICE_USERNAME);
-   console.log(process.env.PRACTICE_PASSWORD);
 
-   console.log(`Username is:${process.env.PRACTICE_USERNAME}`);
-});
-test("Bypass authentication by encoding the credentials base64 format", async ({ page,}) => {
-  let encodedCredential = Buffer.from(`${process.env.PRACTICE_USERNAME}:${process.env.PRACTICE_PASSWORD}`).toString("base64");
-  await page.setExtraHTTPHeaders({Authorization: `Basic ${encodedCredential}`,});
-  page.goto("https://practice.cydeo.com/basic_auth");
-  await page.waitForTimeout(3000);
-});
